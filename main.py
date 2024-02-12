b1f380533bcf7e6be8f587c374589796cb4d5ab4
Qaisar-Mateen <qaisarmateen313@gmail.com>
2024-02-12 21:48:09 +0500

    12-1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import yfinance as yf
+import customtkinter as ctk
 import plotly.graph_objects as graph
 from fbprophet import Prophet
 from fbprophet.plot import plot_plotly, plot_components_plotly
