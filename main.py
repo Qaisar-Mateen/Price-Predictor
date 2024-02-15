24e2beac2e14c69e59bb67c8e78564dfc1a153f8
Qaisar-Mateen <qaisarmateen313@gmail.com>
2024-02-15 23:26:51 +0500

    15-8

--- main.py
+++ main.py
@@ -11,5 +11,5 @@ pd.options.display.float_format = '${:,.2f}'.format #global float format
 # Get the data from Yahoo Finance
 today = datetime.today().strftime('%Y-%m-%d')
 start_date = '2016-01-01'
-eth_df = yf.download('ETH-USD',start_date, today)
+eth_df = yf.download('ETH-USD',start_date, today) 
 eth_df.tail()
