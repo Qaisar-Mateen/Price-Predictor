531ff32149e5566175a6be9236a9589e1bbffe19
Qaisar-Mateen <qaisarmateen313@gmail.com>
2024-02-15 23:15:59 +0500

    15-6

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import yfinance as yf
 import customtkinter as ctk
 import plotly.graph_objects as graph
-from fbprophet import Prophet
-from fbprophet.plot import plot_plotly, plot_components_plotly
+from prophet import model
+from prophet.plot import plot_plotly, plot_components_plotly
 
 pd.options.display.float_format = '${:,.2f}'.format #global float format
