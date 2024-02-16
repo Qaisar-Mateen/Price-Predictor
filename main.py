2b4d077a863e2a5907492baed24682250e99f9b7
Qaisar-Mateen <qaisarmateen313@gmail.com>
2024-02-16 23:04:08 +0500

    tried to run ploty

--- main.py
+++ main.py
@@ -17,3 +17,35 @@ eth_df = yf.download('ETH-USD',start_date, today)
 eth_df.reset_index(inplace=True)
 model_df = eth_df[['Date', 'Close']]
 model_df.rename(columns={'Date':'ds', 'Close':'y'}, inplace=True)
+
+# plot the open price
+
+x = model_df["ds"]
+y = model_df["y"]
+
+fig = graph.Figure()
+
+fig.add_trace(graph.Scatter(x=x, y=y))
+
+# Set title
+fig.update_layout(
+    title_text="Time series plot of Ethereum Open Price",
+)
+
+fig.update_layout(
+    xaxis=dict(
+        rangeselector=dict(
+            buttons=list(
+                [
+                    dict(count=1, label="1m", step="month", stepmode="backward"),
+                    dict(count=6, label="6m", step="month", stepmode="backward"),
+                    dict(count=1, label="YTD", step="year", stepmode="todate"),
+                    dict(count=1, label="1y", step="year", stepmode="backward"),
+                    dict(step="all"),
+                ]
+            )
+        ),
+        rangeslider=dict(visible=True),
+        type="date",
+    )
+)
