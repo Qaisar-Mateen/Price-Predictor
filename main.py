422f62cb51e22b2bacadff588106eaccbe17f3c0
Qaisar-Mateen <qaisarmateen313@gmail.com>
2024-02-16 21:13:59 +0500

    16-1

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import yfinance as yf
-import datetime
 import customtkinter as ctk
 import plotly.graph_objects as graph
-from prophet import model
+from datetime import datetime
+from prophet import models
 from prophet.plot import plot_plotly, plot_components_plotly
 
 pd.options.display.float_format = '${:,.2f}'.format #global float format
@@ -13,3 +13,5 @@ today = datetime.today().strftime('%Y-%m-%d')
 start_date = '2016-01-01'
 eth_df = yf.download('ETH-USD',start_date, today) 
 eth_df.tail()
+
+print(eth_df.head())
