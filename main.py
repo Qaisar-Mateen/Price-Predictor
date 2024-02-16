2d1083c33289505c2ad883b79e028e03a35fc97f
Qaisar-Mateen <qaisarmateen313@gmail.com>
2024-02-16 23:04:34 +0500

    16-7

--- main.py
+++ main.py
@@ -20,32 +20,32 @@ model_df.rename(columns={'Date':'ds', 'Close':'y'}, inplace=True)
 
 # plot the open price
 
-x = model_df["ds"]
-y = model_df["y"]
+# x = model_df["ds"]
+# y = model_df["y"]
 
-fig = graph.Figure()
+# fig = graph.Figure()
 
-fig.add_trace(graph.Scatter(x=x, y=y))
+# fig.add_trace(graph.Scatter(x=x, y=y))
 
-# Set title
-fig.update_layout(
-    title_text="Time series plot of Ethereum Open Price",
-)
+# # Set title
+# fig.update_layout(
+#     title_text="Time series plot of Ethereum Open Price",
+# )
 
-fig.update_layout(
-    xaxis=dict(
-        rangeselector=dict(
-            buttons=list(
-                [
-                    dict(count=1, label="1m", step="month", stepmode="backward"),
-                    dict(count=6, label="6m", step="month", stepmode="backward"),
-                    dict(count=1, label="YTD", step="year", stepmode="todate"),
-                    dict(count=1, label="1y", step="year", stepmode="backward"),
-                    dict(step="all"),
-                ]
-            )
-        ),
-        rangeslider=dict(visible=True),
-        type="date",
-    )
-)
+# fig.update_layout(
+#     xaxis=dict(
+#         rangeselector=dict(
+#             buttons=list(
+#                 [
+#                     dict(count=1, label="1m", step="month", stepmode="backward"),
+#                     dict(count=6, label="6m", step="month", stepmode="backward"),
+#                     dict(count=1, label="YTD", step="year", stepmode="todate"),
+#                     dict(count=1, label="1y", step="year", stepmode="backward"),
+#                     dict(step="all"),
+#                 ]
+#             )
+#         ),
+#         rangeslider=dict(visible=True),
+#         type="date",
+#     )
+# )
