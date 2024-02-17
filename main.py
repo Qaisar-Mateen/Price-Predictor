35a5be49074e9e14df11f243eff11df9a1696197
Qaisar-Mateen <qaisarmateen313@gmail.com>
2024-02-17 14:12:46 +0500

    17-7

--- main.py
+++ main.py
@@ -39,10 +39,9 @@ if __name__ == "__main__":
             rangeselector=dict(
             buttons=list(
                     [
-                        dict(count=1, label="1m", step="month", stepmode="backward"),
-                        dict(count=6, label="6m", step="month", stepmode="backward"),
-                        dict(count=1, label="YTD", step="year", stepmode="todate"),
-                        dict(count=1, label="1y", step="year", stepmode="backward"),
+                        dict(count=1, label="1m", step="month", stepmode="todate"),
+                        dict(count=6, label="6m", step="month", stepmode="todate"),
+                        dict(count=1, label="1y", step="year", stepmode="todate"),
                         dict(step="all"),
                     ]
                 )
