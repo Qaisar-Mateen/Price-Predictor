619c780f15cff312c5bc46a981125a0f87e3dfd6
Qaisar-Mateen <qaisarmateen313@gmail.com>
2024-02-17 19:57:56 +0500

    17-15

--- main.py
+++ main.py
@@ -45,22 +45,130 @@ app.layout = html.Div([
     ),
     dcc.Graph(id='graph')
 ])
+
+xb, yb, model_btc = BTC_data()
+xe, ye, model_eth = Eth_data()
+mb = Prophet(seasonality_mode="multiplicative")
+me = Prophet(seasonality_mode="multiplicative")
+mb.fit(model_btc)
+me.fit(model_eth)
+
+futureb = mb.make_future_dataframe(periods = 365)
+forecastb = mb.predict(futureb)
+
+futuree = me.make_future_dataframe(periods = 365)
+forecaste = me.predict(futuree)
+
+today = datetime.today().strftime('%Y-%m-%d')
+
+previous_trendb = forecastb[forecastb['ds'] < today]
+future_trendb = forecastb[forecastb['ds'] >= today]
+
+previous_trende = forecaste[forecaste['ds'] < today]
+future_trende = forecaste[forecaste['ds'] >= today]
+
+
 @app.callback(Output('graph', 'figure'), Input('dropdown', 'value'))
 def build_graph(selectedValue):
     if selectedValue == 'BTC-USD':
-        x, y, model = BTC_data()
+        fig = graph.Figure(
+            data=[
+                graph.Scatter(x=[], y=[]),
+                graph.Scatter(x=xb, y=yb, mode='lines', name='Actual Open Price', line=dict(color='blue')),
+                graph.Scatter(
+                    x=previous_trendb['ds'], 
+                    y=(previous_trendb['yhat']+previous_trendb['yhat_upper'])/2, 
+                    name='Previous Prediction',
+                    line=dict(color='red')
+                )
+            ],
+            frames=[graph.Frame(
+                data=[graph.Scatter(
+                        x=future_trendb['ds'].iloc[:i],
+                        y=(future_trendb['yhat'].iloc[:i]+future_trendb['yhat_upper'].iloc[:i])/2,
+                        mode='lines',
+                        name='Future Predicted Trend',
+                        line=dict(color='red')
+                    )
+                ]
+            ) for i in range(0, len(future_trendb), 15)],
+            layout=graph.Layout(
+                updatemenus=[
+                    dict(
+                        type="buttons",
+                        buttons=[
+                            dict(
+                                label="Predict Trend",
+                                method="animate",
+                                args=[None, {"frame": {"duration": 500, "redraw": True}, "fromcurrent": True}]
+                            )
+                        ]
+                    )
+                ]
+            )
+        )
     else:
-        x, y, model = Eth_data()
+        fig = graph.Figure(
+            data=[
+                graph.Scatter(x=[], y=[]),
+                graph.Scatter(x=xe, y=ye, mode='lines', name='Actual Open Price', line=dict(color='blue')),
+                graph.Scatter(
+                    x=previous_trende['ds'], 
+                    y=(previous_trende['yhat']+previous_trende['yhat_upper'])/2, 
+                    name='Previous Prediction',
+                    line=dict(color='red')
+                )
+            ],
+            frames=[graph.Frame(
+                data=[graph.Scatter(
+                        x=future_trende['ds'].iloc[:i],
+                        y=(future_trende['yhat'].iloc[:i]+future_trende['yhat_upper'].iloc[:i])/2,
+                        mode='lines',
+                        name='Future Predicted Trend',
+                        line=dict(color='red')
+                    )
+                ]
+            ) for i in range(0, len(future_trende), 15)],
+            layout=graph.Layout(
+                updatemenus=[
+                    dict(
+                        type="buttons",
+                        buttons=[
+                            dict(
+                                label="Predict Trend",
+                                method="animate",
+                                args=[None, {"frame": {"duration": 500, "redraw": True}, "fromcurrent": True}]
+                            )
+                        ]
+                    )
+                ]
+            )
+        )
 
-    m = Prophet(seasonality_mode="multiplicative")
-    m.fit(model)
+    # Set title
+    fig.update_layout(
+        title_text="Trend Prediction of Ethereum" if selectedValue == 'ETH-USD' else "Trend Prediction of Bitcoin", 
+        xaxis_title="Date", 
+        yaxis_title="Price (USD)"
+    )
 
-    future = m.make_future_dataframe(periods = 365)
-    forecast = m.predict(future)
-    today = datetime.today().strftime('%Y-%m-%d')
-
-    previous_trend = forecast[forecast['ds'] < today]
-    future_trend = forecast[forecast['ds'] >= today]
+    fig.update_layout(
+        xaxis=dict(
+            rangeselector=dict(
+            buttons=list(
+                    [
+                        dict(count=1, label="1m", step="month", stepmode="todate"),
+                        dict(count=6, label="6m", step="month", stepmode="todate"),
+                        dict(count=1, label="1y", step="year", stepmode="todate"),
+                        dict(step="all"),
+                    ]
+                )
+            ),
+            rangeslider=dict(visible=True),
+            type="date",
+        )
+    )
+    else:        
     fig = graph.Figure(
         data=[
             graph.Scatter(x=[], y=[]),
@@ -90,7 +198,7 @@ def build_graph(selectedValue):
                         dict(
                             label="Predict Trend",
                             method="animate",
-                            args=[None]
+                            args=[None, {"frame": {"duration": 500, "redraw": True}, "fromcurrent": True}]
                         )
                     ]
                 )
